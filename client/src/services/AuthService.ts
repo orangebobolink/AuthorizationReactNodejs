f079503f98b4400caad8059f0ff9e6e958944320
Osten Power <osten_power@mail.ru>
2023-04-02 16:50:23 +0300

    completed server, start client

---
+++ client/src/services/AuthService.ts
@@ -0,0 +1,18 @@
+import $api from "../http";
+import {AxiosResponse} from "axios";
+import {AuthRepsponse} from "../models/response/AuthRepsponse";
+
+export default class AuthService {
+    static async login(email: string, password: string): Promise<AxiosResponse<AuthRepsponse>> {
+        return $api.post<AuthRepsponse>('/login', {email, password})
+    }
+
+    static async registration(email: string, password: string): Promise<AxiosResponse<AuthRepsponse>> {
+        return $api.post<AuthRepsponse>('/login', {email, password})
+    }
+
+    static async logout(): Promise<void> {
+        return $api.post('/logout')
+    }
+}
+
--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,50 +1,91 @@
-const userService = require("../service/user-service");
+const userService = require('../service/user-service');
+const {validationResult} = require('express-validator');
+const ApiError = require('../exceptions/api-error');
 
 class UserController {
-  async registration(req, res, next) {
-    try {
-      const { email, password } = req.body;
-      const userData = await userService.registration(email, password);
+    async registration(req, res, next) {
+        try {
+            const errors = validationResult(req);
 
-      res.cookie("refreshToken", userData.refreshToken, {
-        maxAge: 30 * 25 * 60 * 60 * 1000,
-        httpOnly: true,
-      });
-      return res.json(userData);
-    } catch (e) {
-      console.log(e);
+            if (!errors.isEmpty()) {
+                return next(
+                    ApiError.BadRequest('Ошибка при валидации', errors.array())
+                );
+            }
+
+            const {email, password} = req.body;
+            const userData = await userService.registration(email, password);
+
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 25 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
     }
-  }
 
-  async login(req, res, next) {
-    try {
-    } catch (e) {}
-  }
+    async login(req, res, next) {
+        try {
+            const {email, password} = req.body;
+            const userData = await userService.login(email, password);
 
-  async logout(req, res, next) {
-    try {
-    } catch (e) {}
-  }
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 25 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
+    }
 
-  async activate(req, res, next) {
-    try {
-      const activationLink = req.params.link;
-      await userService.activate(activationLink);
+    async logout(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies;
+            const token = await userService.logout(refreshToken)
+            res.clearCookie('refreshToken')
+            return res.json(token)
+        } catch (e) {
+            next(e);
+        }
+    }
 
-      return res.redirect(process.env.CLIENT_URL);
-    } catch (e) {}
-  }
+    async activate(req, res, next) {
+        try {
+            const activationLink = req.params.link;
+            await userService.activate(activationLink);
 
-  async refresh(req, res, next) {
-    try {
-    } catch (e) {}
-  }
+            return res.redirect(process.env.CLIENT_URL);
+        } catch (e) {
+            next(e);
+        }
+    }
 
-  async getUsers(req, res, next) {
-    try {
-      res.json(["123", "456"]);
-    } catch (e) {}
-  }
+    async refresh(req, res, next) {
+        try {
+            const {refreshToken} = req.cookies
+            const userData = await userService.refresh(refreshToken);
+
+            res.cookie('refreshToken', userData.refreshToken, {
+                maxAge: 30 * 25 * 60 * 60 * 1000,
+                httpOnly: true,
+            });
+            return res.json(userData);
+        } catch (e) {
+            next(e);
+        }
+    }
+
+    async getUsers(req, res, next) {
+        try {
+            const users = await userService.getAllUsers()
+            return res.json(users);
+        } catch (e) {
+            next(e);
+        }
+    }
 }
 
 module.exports = new UserController();
